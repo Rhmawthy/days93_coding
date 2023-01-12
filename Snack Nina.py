7763293b2b29888a062ed87802fb36a73a1914f7
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2023-01-12 16:09:29 +0800

    Add files via upload

---
+++ Snack Nina.py
@@ -0,0 +1,10 @@
+""" di hari ulang tahun Nina, ia ingin membagikan snack kepada temannya, berapa banyak jumlah snack yang di bagikan ke temannya jika :
+	jumlah teman berupa inputan
+	snack yg di beri ibunya 100 pcs	
+	snack yang di beli Nina 20 pcs """
+	
+teman = int (input("jumlah teman Nina "))
+
+jumlah = (100 + 20) /teman	
+
+print("jumlah snack perteman",jumlah)		
